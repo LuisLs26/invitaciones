d9455422f6b9f11ce2dad4377b4e4c48e54bedec
Luis <luisnurenagonzales@gmail.com>
2026-08-13 14:52:02 -0500

    Agregar cliente luisnurenagonzales y nuevas plantillas

---
+++ clientes/luisnurenagonzales/codigo/config.js
@@ -0,0 +1,74 @@
+/**
+ * Configuración Personalizada: Luis y Kiara
+ */
+const INVITATION_CONFIG = {
+    "id": "boda",
+    "type": "boda",
+    "theme": "theme-boda",
+    "personName": "Luis y Kiara",
+    "title": "Nuestra Boda",
+    "subtitle": "Con la bendición de Dios y nuestros padres",
+    "date": "2026-08-26T16:30",
+    "formattedDate": "Miércoles, 26 de Agosto de 2026",
+    "time": "4:30 PM (Ceremonia Religiosa)",
+    "locationName": "Iglesia San Francisco & Casona Los Olivos",
+    "address": "Jr. Lampa 210, Centro Histórico / Av. Primavera 1200",
+    "mapUrl": "https://maps.google.com/?q=Iglesia+San+Francisco+Lima",
+    "whatsapp": "51988799404",
+    "whatsappMessage": "¡Hola Luis y Kiara! Confirmo mi asistencia a su Boda. Nombre(s): ",
+    "heroImage": "../imagenes/hero.jpg",
+    "coverQuote": "El amor no es mirarse el uno al otro, sino mirar juntos en la misma dirección.",
+    "mainMessage": "Después de caminar juntos y construir una historia hermosa, hemos decidido unir nuestras vidas en matrimonio. Nos encantaría contar con tu presencia en este momento tan especial.",
+    "dressCode": "Rigurosa Etiqueta / Traje Oscuro y Vestido Largo",
+    "giftInfo": "Lo más importante es tu compañía. Si deseas obsequiarnos algo, agradeceremos lluvia de sobres en recepción.",
+    "timeline": [
+        {
+            "time": "4:30 PM",
+            "title": "Ceremonia Religiosa",
+            "description": "Parroquia San Francisco de Asís"
+        },
+        {
+            "time": "6:30 PM",
+            "title": "Coctel de Bienvenida",
+            "description": "Jardines de la Casona Los Olivos"
+        },
+        {
+            "time": "7:30 PM",
+            "title": "Cena de Gala & Brindis",
+            "description": "Brindis de novios y palabras de honor"
+        },
+        {
+            "time": "9:00 PM",
+            "title": "Fiesta & Hora Loca",
+            "description": "Banda en vivo y celebración de noche"
+        }
+    ],
+    "padrinos": [
+        {
+            "role": "Padres de la Novia",
+            "name": "Fernando Salazar & Carmen de Salazar"
+        },
+        {
+            "role": "Padres del Novio",
+            "name": "Jorge Carlos & Beatriz Morales"
+        }
+    ],
+    "showCountdown": true,
+    "showGallery": true,
+    "gallery": [
+        {
+            "url": "../imagenes/galeria1.jpg",
+            "caption": "Nuestra Historia de Amor"
+        },
+        {
+            "url": "../imagenes/galeria2.jpg",
+            "caption": "El Compromiso & Anillos"
+        },
+        {
+            "url": "../imagenes/galeria3.jpg",
+            "caption": "Noche de Fiesta & Recepción"
+        }
+    ],
+    "showMap": true,
+    "finalMessage": "Esperamos compartir esta gran dicha contigo. ¡Te esperamos!"
+};
